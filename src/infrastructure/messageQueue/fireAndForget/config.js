55955a005e2a43802475a6461aff356aae074820
MO7G <mr.mohammed1913@gmail.com>
2025-02-22 20:27:41 +0200

    deleting url

--- src/infrastructure/messageQueue/fireAndForget/config.js
+++ src/infrastructure/messageQueue/fireAndForget/config.js
@@ -1,6 +1,6 @@
 module.exports = {
     rabbitMQ: {
-        url: "amqps://eduai:QPCJBimzAA!VDk2haijT@b-ae976da2-2915-4eb9-9de3-e0ed71858096.mq.eu-north-1.amazonaws.com:5671", // Change to your RabbitMQ server URL
+        url: "amqps://eduai:pass-here@b-ae976da2-2915-4eb9-9de3-e0ed71858096.mq.eu-north-1.amazonaws.com:5671", // Change to your RabbitMQ server URL
         queues: ["user_task_queue", "user_event_queue"], // Define your queues here
     }
 };
